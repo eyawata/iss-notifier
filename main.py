36c472bd99b38978db44c1233e42fe5ca2092ef9
Eri Yawata <yawata.eri@gmail.com>
2024-12-13 18:23:35 +0900

    add boilerplate

--- main.py
+++ main.py
@@ -2,36 +2,34 @@ from datetime import datetime
 
 import requests
 
-# Mt. Fuji Coordinates
+MY_LAT = 51.507351  # Your latitude
+MY_LONG = -0.127758  # Your longitude
 
-fuji_lat = 35.363602
-fuji_long = 138.726379
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+# Your position is within +5 or -5 degrees of the ISS position.
+
 
 parameters = {
-    "lat": fuji_lat,
-    "lng": fuji_long,
-    "tzid": "Asia/Tokyo",
+    "lat": MY_LAT,
+    "lng": MY_LONG,
     "formatted": 0,
 }
 
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-
+response.raise_for_status()
 data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
 time_now = datetime.now()
 
-
-# ISS API FETCH
-# response = requests.get("http://api.open-notify.org/iss-now.json")
-# response.raise_for_status()
-
-# data = response.json()
-
-# position = data["iss_position"]
-# latitude = position["latitude"]
-# longitude = position["longitude"]
-
-# coordinates = (latitude, longitude)
-
-# print(coordinates)
+# If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
---
+++ test.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+import requests
+
+# Mt. Fuji Coordinates
+
+fuji_lat = 35.363602
+fuji_long = 138.726379
+
+parameters = {
+    "lat": fuji_lat,
+    "lng": fuji_long,
+    "tzid": "Asia/Tokyo",
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+time_now = datetime.now()
+
+# ISS API FETCH
+# response = requests.get("http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# data = response.json()
+
+# position = data["iss_position"]
+# latitude = float(position["latitude"])
+# longitude = float(position["longitude"])
+
+# coordinates = (latitude, longitude)
+
+# print(coordinates)
